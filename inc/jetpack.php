425406ffaab27c9ef019c4cbba252b102c2bf2e5
David Shanske <david@shanske.com>
2017-07-08 21:43:21 -0500

    Update jetpack file

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -1,19 +1,54 @@
 <?php
 /**
  * Jetpack Compatibility File
- * See: http://jetpack.me/
  *
- * @package MF2_S
+ * @link https://jetpack.com/
+ *
+ * @package mf2_s
  */
 
 /**
- * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
  */
 function mf2_s_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
+		'render'    => 'mf2_s_infinite_scroll_render',
 		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Content Options.
+	add_theme_support( 'jetpack-content-options', array(
+		'post-details' => array(
+			'stylesheet' => '_s-style',
+			'date'       => '.posted-on',
+			'categories' => '.cat-links',
+			'tags'       => '.tags-links',
+			'author'     => '.byline',
+			'comment'    => '.comments-link',
+		),
 	) );
 }
 add_action( 'after_setup_theme', 'mf2_s_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function mf2_s_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
