d41dd5e4b67530a666b9bef1f7b37ceda3d4ff7c
David Shanske <david@shanske.com>
2017-07-08 21:44:04 -0500

    Update Header from upstream

--- header.php
+++ header.php
@@ -1,69 +1,54 @@
 <?php
 /**
- * The header for our theme.
+ * The header for our theme
  *
- * Displays all of the <head> section and everything up till <div id="content">
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
- * @package MF2_S
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
  */
-?><!DOCTYPE html>
+
+?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="profile" href="http://microformats.org/profile/specs" />
-<link rel="profile" href="http://microformats.org/profile/hatom" />
+
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); mf2_s_semantics("body"); ?> >
+<body <?php body_class(); ?>>
 <div id="page" class="site">
-	<?php if (!is_singular() ) 
-            {
-		// H-Feed P-Name
-		echo '<span class="p-name hfeedname">';
-		echo wp_title();
-		echo ' - Feed</span>';
-	    }
-	?>
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mf2_s' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
+	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-        $title = mf2_s_semantics("site-title", false) . '><a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" rel="home" ' . mf2_s_semantics("site-url", false) . '>' . get_bloginfo( 'name' ) . '</a>';
-				if ( get_header_image() ) { ?>
-    			<span class="custom-header">
-    			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" <?php mf2_s_semantics("site-url"); ?> rel="home">
-    			<img src="<?php header_image(); ?>" width="100%" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt=<?php echo esc_attr( get_bloginfo( 'name', 'display') );?>>
-    			</a>
-  				</span>
-  		<?php } // End header image check. 
-			  else {
-					if ( is_front_page() && is_home() ) {
-						echo '<h1 ' . $title . '</h1>';
-				  } 
-					else { 
-						echo '<h2 ' . $title . '</h2>';				
-					}
-				}
-			$description = get_bloginfo( 'description' );
-			if ( ! empty( $description ) ) : ?>
-				<div class="site-description" <?php mf2_s_semantics("site-description"); ?>><?php echo esc_html( $description ); ?></div>
-			<?php endif;
-			?>
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title p-name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+			<?php
+			endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"></button>
-		<?php
-			$args = array(
-				'theme_location'  => 'primary',
-        'menu_id' => 'primary-menu'
-);
-			wp_nav_menu( $args ); 
-		?>
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
+			<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
